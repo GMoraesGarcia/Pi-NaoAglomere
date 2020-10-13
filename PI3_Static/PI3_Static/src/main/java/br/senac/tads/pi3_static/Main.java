713fb6515afdc706619015ac3b47863902af0717
MatheusSantosDev <matheus.c.santos2@gmail.com>
2020-10-13 20:02:43 -0300

    teste do primeiro commit

---
+++ PI3_Static/PI3_Static/src/main/java/br/senac/tads/pi3_static/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3_static;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("b-b-b");
+    }
+}
