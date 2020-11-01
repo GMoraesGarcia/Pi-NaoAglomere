8aa82cdfc7f8f0d6132924fcbb7b161cf160b862
MatheusSantosDev <matheus.c.santos2@gmail.com>
2020-10-31 22:13:09 -0300

    correção redirect index.html

--- PI3_Static_NaoAglomere2/src/main/java/index/indexHtmlServlet.java
+++ PI3_Static_NaoAglomere2/src/main/java/index/indexHtmlServlet.java
@@ -1,24 +1,19 @@
 package index;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "indexHtmlServlet", urlPatterns = {""})
 public class indexHtmlServlet extends HttpServlet {
 
-    
-       
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {       
+            throws ServletException, IOException {
         response.sendRedirect("home-abrir");
-    }     
-    
+    }
+
 }
